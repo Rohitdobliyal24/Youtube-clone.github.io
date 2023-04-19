4019ddeba433de4d02a9ce8be01810c7f756733a
Rohitdobliyal24 <rohitdobliyal24@gmail.com>
2023-04-19 10:59:20 +0530

    youtube search api change

--- src/utils/Constants.js
+++ src/utils/Constants.js
@@ -2,7 +2,7 @@ export const GOOGLE_API_KEY="AIzaSyDen8XV1QsDSAu9BaEgyzG98ien34I1Zqk";
 
 export const YOUTUBE_VIDEOS_API="https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key=" + GOOGLE_API_KEY;
 
-export const YOUTUBE_SEARCH_API="http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q="
+export const YOUTUBE_SEARCH_API="https://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q="
 //youtube video api mai change kiya hai
 
 export const COMMENT_API="https://youtube.googleapis.com/youtube/v3/commentThreads?part=snippet%2Creplies&key="+GOOGLE_API_KEY+"&maxResults=100&videoId=";
